a1161dbde2f0b4332f05a4bf75e7f4e1a5795727
surik.v <92221830+ivan-s-s@users.noreply.github.com>
2023-11-10 00:30:10 +0500

    Add counter (#3)

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import classes from './Counter.module.css';
+
+const Counter = (props) => {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    function decrement() {
+        setCount(count - 1);
+    }
+
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={increment} className={classes.myButton}>+</button>
+            <button onClick={decrement} className={classes.myButton}>-</button>
+        </div>
+    );
+}
+
+export default Counter;
