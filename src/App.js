3cbb2372f7c293e5f558443acd83f24356011abf
Leo Kotman <71670937+leokotman@users.noreply.github.com>
2023-11-11 17:28:19 +0100

    Add views state changing (#8)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import { useState } from 'react';
 import './App.css';
-import { Home, Settings, Game } from './views';
+import { Home, Settings, Game, Results } from './views';
+import { PAGES } from './utils/constants';
 
 function App() {
-  const [isHomeOpen, setIsHomeOpen] = useState(true);
-  const [isSettingsOpen, setIsSettingsOpen] = useState(true);
-  const [isGameOpen, setIsGameOpen] = useState(true);
+  const [location, setLocation] = useState(PAGES.Home);
+  const isHomeOpen = location === PAGES.Home;
+  const isSettingsOpen = location === PAGES.Settings;
+  const isGameOpen = location === PAGES.Game;
+  const isResultsOpen = location === PAGES.Results;
+
+  const goToNextPage = (viewToOpen) => {
+    setLocation(viewToOpen);
+  };
 
   return (
     <div className="App">
-      {isHomeOpen && <Home />}
-      {isSettingsOpen && <Settings />}
-      {isGameOpen && <Game />}
+      {isHomeOpen && <Home onClickPlay={() => goToNextPage(PAGES.Settings)} />}
+      {isSettingsOpen && (
+        <Settings onClickStartGame={() => goToNextPage(PAGES.Game)} />
+      )}
+      {isGameOpen && (
+        <Game
+          onRestartGame={() => goToNextPage(PAGES.Home)}
+          onFinishGame={() => goToNextPage(PAGES.Results)}
+        />
+      )}
+      {isResultsOpen && (
+        <Results onRestartGame={() => goToNextPage(PAGES.Home)} />
+      )}
     </div>
   );
 }
