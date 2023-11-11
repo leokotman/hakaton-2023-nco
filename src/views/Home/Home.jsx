932f472d72a278931683dfb0372187e2f8ef8153
IvanS <i.surovtsev22@gmail.com>
2023-11-12 03:33:45 +0500

    Update start page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,3 +1,5 @@
+import startImg from '../../assets/images/start.png';
+import GameTitle from '../../components/GameTitle/GameTitle';
 import classes from './Home.module.css';
 
 export const Home = (props) => {
@@ -5,8 +7,15 @@ export const Home = (props) => {
 
   return (
     <div className={classes.home}>
-      Найди пару (заголовок)
-      <button onClick={onClickPlay}>Начать игру</button>
+      <div className={classes.gradient}></div>
+      <img src={startImg} className={classes.startImg} alt="start" />
+      <GameTitle />
+      <button className={classes.start} onClick={onClickPlay}>
+        <div className={classes.playIcon}>
+            <div className={classes.play}></div>
+        </div>
+        Начать игру
+      </button>
     </div>
   );
 };
