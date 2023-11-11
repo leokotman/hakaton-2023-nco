44d4caf5b3eea0a2e2b7fb799adbab89b3976352
Leo Kotman <71670937+leokotman@users.noreply.github.com>
2023-11-11 17:28:19 +0100

    Add views state changing (#8)

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,21 +2,25 @@ import React, { useState } from 'react';
 import classes from './Counter.module.css';
 
 export const Counter = (props) => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    function increment() {
-        setCount(count + 1);
-    }
+  function increment() {
+    setCount(count + 1);
+  }
 
-    function decrement() {
-        setCount(count - 1);
-    }
+  function decrement() {
+    setCount(count - 1);
+  }
 
-    return (
-        <div>
-            <h2>{count}</h2>
-            <button onClick={increment} className={classes.myButton}>+</button>
-            <button onClick={decrement} className={classes.myButton}>-</button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={increment} className={classes.myButton}>
+        +
+      </button>
+      <button onClick={decrement} className={classes.myButton}>
+        -
+      </button>
+    </div>
+  );
+};
--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -1,25 +1,18 @@
 import classes from './Game.module.css';
 import { Navbar, Counter } from '../../components';
 
-export const Game = () => {
+export const Game = (props) => {
+  const { onRestartGame, onFinishGame } = props;
+
   return (
     <>
-    <header className="App-header">
-        <Navbar title='Hakaton' />
+      <header className="App-header">
+        <Navbar title="Hakaton" />
         <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onRestartGame}>Начать сначала</button>
+        <p>Игра доска</p>
+        <button onClick={onFinishGame}>Закончить игру</button>
       </header>
-
     </>
-  )
-}
+  );
+};
