59fc2e23ca833ddb60e1428015fdffdc05678d49
leokot <leoktmn@yandex.ru>
2023-11-11 22:56:29 +0100

    add firebase to project

--- src/views/Settings/Settings.jsx
+++ src/views/Settings/Settings.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
+
 import classes from './Settings.module.css';
+import { handleSaveUser } from '../../utils/dbFunctions';
 
 export const Settings = (props) => {
   const { onClickStartGame } = props;
+  const [userName, setUserName] = useState('');
+
   return (
     <div className={classes.settings}>
       Настройки (выбери уровень)
-      <button onClick={onClickStartGame}>Начать игру</button>
+      <input
+        type="text"
+        placeholder="Ваше имя в игре"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          onClickStartGame();
+          handleSaveUser(userName);
+        }}
+      >
+        Начать игру
+      </button>
     </div>
   );
 };
