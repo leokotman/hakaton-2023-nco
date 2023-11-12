cc9be076f5800edad0bce29e6c2a064ce3241447
IvanS <i.surovtsev22@gmail.com>
2023-11-12 17:56:17 +0500

    Update pause page

--- src/views/Pause/Pause.jsx
+++ src/views/Pause/Pause.jsx
@@ -5,19 +5,20 @@ export const Pause = (props) => {
 
   return (
     <div className={classes.home}>
-      Найди пару (заголовок)
-      <div className={classes.modalContainer}>
-      <fieldset className={classes.modal}>
-        <legend className={classes.legend}>ПАУЗА</legend>
-        <p className={classes.question}>Вернуться к игре</p>
-        <button className={classes.playBtn} onClick={onClickPlay}>
-          <div className={classes.playIcon}>
-            <div className={classes.play}></div>
-          </div>
-        </button>
-      </fieldset>
-    </div>
-      
+      <div className={classes.gradient}>
+        <div className={classes.modalContainer}>
+          <h1 className={classes.title}>Найди пару</h1>
+          <fieldset className={classes.modal}>
+            <legend className={classes.legend}>ПАУЗА</legend>
+            <p className={classes.question}>Вернуться к игре</p>
+            <button className={classes.playBtn} onClick={onClickPlay}>
+              <div className={classes.playIcon}>
+                <div className={classes.play}></div>
+              </div>
+            </button>
+          </fieldset>
+        </div>
+      </div>
     </div>
   );
 };
