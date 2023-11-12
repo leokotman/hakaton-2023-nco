8b997ef7bf4439d35eae9e1e38b2ee02fcd89ba4
surik.v <92221830+ivan-s-s@users.noreply.github.com>
2023-11-12 18:07:53 +0500

    Update pause page (#17)
    * Add pause style
    
    * Add button
    
    * Add playicon
    
    * Fix play btn styles
    
    * Add some structure for page
    
    * Add styles
    
    * Update pause page

--- src/views/Pause/Pause.jsx
+++ src/views/Pause/Pause.jsx
@@ -5,8 +5,20 @@ export const Pause = (props) => {
 
   return (
     <div className={classes.home}>
-      Найди пару (заголовок)
-      <button onClick={onClickPlay}>Вернуться к игре</button>
+      <div className={classes.gradient}>
+        <div className={classes.modalContainer}>
+          <h1 className={classes.title}>Найди пару</h1>
+          <fieldset className={classes.modal}>
+            <legend className={classes.legend}>ПАУЗА</legend>
+            <p className={classes.question}>Вернуться к игре</p>
+            <button className={classes.playBtn} onClick={onClickPlay}>
+              <div className={classes.playIcon}>
+                <div className={classes.play}></div>
+              </div>
+            </button>
+          </fieldset>
+        </div>
+      </div>
     </div>
   );
 };
