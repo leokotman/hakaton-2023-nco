b37bfe181c4f5e2a4ea7024b68b1fac683b3342d
IvanS <i.surovtsev22@gmail.com>
2023-11-12 12:55:05 +0500

    Add confirm massage
    Cancel integration
    
    Integrate confirm component
    
    Fix back button
    
    Upgrade css style

--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -1,15 +1,26 @@
 import { Navbar, Board } from 'components';
 import classes from './Game.module.css';
+import { useState } from 'react';
+import Confirm from 'components/Confirm/Confirm';
 
 export const Game = (props) => {
   const { onRestartGame, onPauseGame } = props;
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
   return (
     <>
       <header className="App-header">
+        <div>
+          {modalIsOpen && (
+            <Confirm onRestartGame={onRestartGame} onClickPlay={closeModal} />
+          )}
+        </div>
         <Navbar className={classes.title} title="Найди пару" />
         <Board />
-        <button className={classes.restart} onClick={onRestartGame}></button>
+        <button className={classes.restart} onClick={openModal}></button>
         <button className={classes.pause} onClick={onPauseGame}></button>
       </header>
     </>
