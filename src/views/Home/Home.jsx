1da15a339d0f9ce61d1e2c4c59980dcaefaaedfd
surik.v <92221830+ivan-s-s@users.noreply.github.com>
2023-11-12 16:12:11 +0500

    Update start page (#14)
    * Update start page
    
    * Update style module
    
    * Add title component
    
    * Add images
    
    * Fix gradient repeat
    
    * Move start container
    
    * Udpate styles css
    
    * Add shining

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,3 +1,5 @@
+import startImg from '../../assets/images/start.png';
+import GameTitle from '../../components/GameTitle/GameTitle';
 import classes from './Home.module.css';
 
 export const Home = (props) => {
@@ -5,8 +7,19 @@ export const Home = (props) => {
 
   return (
     <div className={classes.home}>
-      Найди пару (заголовок)
-      <button onClick={onClickPlay}>Начать игру</button>
+      <div className={classes.gradient}></div>
+      <img src={startImg} className={classes.startImg} alt="start" />
+      <div className={classes.startContainer}>
+        <div className={classes.startBox}>
+          <GameTitle />
+          <div className={classes.start}>Начать игру</div>
+        </div>
+        <button className={classes.playBtn} onClick={onClickPlay}>
+          <div className={classes.playIcon}>
+              <div className={classes.play}></div>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
