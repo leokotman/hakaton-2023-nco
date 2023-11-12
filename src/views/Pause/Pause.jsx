88b03d75228045fdddc612ec6a97a9f8a76e7a7a
IvanS <i.surovtsev22@gmail.com>
2023-11-12 16:21:54 +0500

    Add some structure for page

--- src/views/Pause/Pause.jsx
+++ src/views/Pause/Pause.jsx
@@ -7,6 +7,14 @@ export const Pause = (props) => {
     <div className={classes.home}>
       Найди пару (заголовок)
       Вернуться к игре
+      <div className={classes.modalContainer}>
+      <fieldset className={classes.modal}>
+        <legend className={classes.legend}>Предупреждение</legend>
+        <p className={classes.question}>Вы точно хотите начать заново?</p>
+        <button onClick={onRestartGame}>новая игра</button>
+        <button onClick={onClickPlay}>вернуться</button>
+      </fieldset>
+    </div>
       <button className={classes.playBtn} onClick={onClickPlay}>
         <div className={classes.playIcon}>
           <div className={classes.play}></div>
